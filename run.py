020f5f9ea70b745068935d7b88253b86f7929930
GigiLi89 <li.gigi89@gmail.com>
2024-04-01 08:02:18 +0000

    Add intro, small madlibs game and outro/play again request

--- run.py
+++ run.py
@@ -1,3 +1,28 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+# Welcome message and request to play for user
+print("Welcome to this Madlibs game!")
+
+play = input("Lets play, shall we? yes/no \n")
+procees="yes" or "no"
+if play == "yes":
+    print("Alright, let's get started! \n")
+else:
+    print("Ok, lets play another time.")
+    exit()
+
+# Inputs for the user 
+a = input("Enter your name: ")
+b = input("Enter a friends name: ")
+c = input("Enter a number: ")
+
+# The story with the inputs
+print("One summer day in June, " + a + " surprised " + b + " with " + c + " puppies.\n")
+
+play_again = input("Would you like to play another Madlibs game? yes/no \n")
+process_again="yes" or "no"
+if play_again == "yes":
+    print("Woho, let's do this! \n")
+else:
+    print("Alright, thanks for playing!")
+    exit()
+
